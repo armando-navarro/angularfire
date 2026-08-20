188d9cc226b4ceb5e5a2c8c252ff0ddc281cf7c2
Armando Navarro <adnavarro@google.com>
2026-08-20 09:45:25 -0700

    feat(recipe-demo): add Firebase web config

---
+++ recipe-demo/src/app/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAt9lVugjwr0wpQeke0UXv3sOunlVDe94E',
+  authDomain: 'recipe-demo-97859.firebaseapp.com',
+  projectId: 'recipe-demo-97859',
+  appId: '1:241663837838:web:27af061c81c4efec332bdf',
+};
+
+export const recaptchaSiteKey = '6Le0MJUtAAAAACt4HYN9l8GD7z23YibH6pPqlFQF';
